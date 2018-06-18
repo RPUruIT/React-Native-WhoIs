975ba4590ab587c2686b8d2c725d1c91c601c2af
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2018-06-18 13:06:09 -0300

    ya esta sacando y guardando la foto en ambas plataformas en android, se me tranca la camara y me hace un ruido de metralleta que mete miedo

--- components/MyCamera.js
+++ components/MyCamera.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppRegistry,View,StyleSheet} from 'react-native';
+import {AppRegistry,View,Text,StyleSheet} from 'react-native';
 
 import Camera from "react-native-camera"
 
@@ -13,16 +13,50 @@ const styles = StyleSheet.create({
       justifyContent:"flex-end",
       alignItems:"center"
     },
+    capture:{
+        flex: 0,
+        backgroundColor:'white',
+        borderRadius:10,
+        color:'#1ed760',
+        padding:15,
+        margin:45
+    }
 })
 
 export default class MyCamera extends React.Component{
+
+    takePicture(){
+        try {
+            const options = {
+                /*audio: false,
+                mode: Camera.constants.CaptureMode.still,
+                target: Camera.constants.CaptureTarget.disk*/
+              };
+            this.camera.capture()
+                .then((data) => console.log(data))
+                .catch(err => console.error(err));
+        } catch (error) {
+            
+        }
+        
+    }
 
     render(){
         return (
             <View style={styles.container}>
                 <Camera ref={(cam)=>{this.camera=cam}} 
                         style={styles.view} 
-                        aspect={Camera.constants.Aspect.fill}></Camera>
+                        aspect={Camera.constants.Aspect.fill}
+                        //type={Camera.constants.Type.front}
+                        captureQuality={Camera.constants.CaptureQuality.medium}
+                        //captureQuality={Camera.constants.CaptureQuality["1080p"]}
+                        >
+                        <Text 
+                            style={styles.capture}
+                            onPress={this.takePicture.bind(this)}>
+                            WHISKY!!
+                        </Text>
+                </Camera>
             </View>
         )
     }
